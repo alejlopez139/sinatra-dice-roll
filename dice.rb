002e1a9acc2190f33d84e26055bb0309de01c4fe
Alejandro Lopez <33030244+alejlopez139@users.noreply.github.com>
2023-09-20 04:27:01 +0000

    added erb for two six dice

--- dice.rb
+++ dice.rb
@@ -2,27 +2,18 @@ require "sinatra"
 require "sinatra/reloader"
 
 get ("/") do
-  "<body><h1>Dice Roll</h1>
-  <ul>
-    <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
-    <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
-    <li><a href=\"/dice/1/20\">Roll one 20-sided dice</a></li>
-    <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
-  </ul>
-  </body>"
+  erb(:elephant)
 end
 
 
 get("/dice/2/6") do
   first_die = rand(1..6)
   second_die = rand(1..6)
-
   sum = first_die + second_die
 
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 
-  "<h1>2d6</h1>
-  <p>#{outcome}</p>"
+  erb(:two_six)
 end
 
 get ("/dice/2/10") do
